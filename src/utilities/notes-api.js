5d89f9d8e0278064e75680a42db5cde22e2591f8
odalysruano <odalys.r.ruano@gmail.com>
2024-04-17 16:32:10 -0400

    implement note api utility

---
+++ src/utilities/notes-api.js
@@ -0,0 +1,10 @@
+import sendRequest from './send-request';
+const BASE_URL = '/api/notes';
+
+export function index() {
+    return sendRequest(BASE_URL, 'GET');
+}
+
+export function create(note) {
+    return sendRequest(BASE_URL, 'POST', note);
+}
